3e2a61d9a1905920493c91f2f8ca7d00d7f0247b
Safayat <safayathossain40@gmail.com>
2024-03-09 22:18:41 +0600

    Initial commit

---
+++ lib/ToDoList/todo_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ToDoListApp extends StatefulWidget {
+  const ToDoListApp({super.key});
+
+  @override
+  State<ToDoListApp> createState() => _ToDoListAppState();
+}
+
+class _ToDoListAppState extends State<ToDoListApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
