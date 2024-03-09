07a74eecbaa24c11bbe54bbdccba76dfeceacf43
Safayat <safayathossain40@gmail.com>
2024-03-09 22:37:56 +0600

    blank

--- lib/ToDoList/todo_list.dart
+++ lib/ToDoList/todo_list.dart
@@ -7,8 +7,44 @@ class ToDoListApp extends StatefulWidget {
 }
 
 class _ToDoListAppState extends State<ToDoListApp> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('TODO LIST APP'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+                'Add a new Task:',
+              style: TextStyle(fontSize:18.0 ),
+            ),
+            const SizedBox(height: 10,),
+            TextFormField(
+              controller: _controller,
+              onTap:()
+                {
+
+
+                },
+
+            ),
+            const SizedBox(height: 20,),
+            const Text(
+              'Tasks:',
+              style: TextStyle(fontSize:19.0, fontWeight: FontWeight.bold),
+
+            ),
+          ],
+        ),
+      ),
+    );
+
   }
 }
